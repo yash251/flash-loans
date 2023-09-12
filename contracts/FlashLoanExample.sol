bbe1eddb0bc4f42828459893ac1dcbc95b407fd7
Yash Jagtap <jagtapyash2512@gmail.com>
2023-09-12 20:40:00 +0700

    contract blpt

---
+++ contracts/FlashLoanExample.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "@aave/core-v3/contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract FlashLoanExample is FlashLoanSimpleReceiverBase {
+    
+}
